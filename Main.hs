6b8253503eaa7f178a6bcb8038adb700fb1dc80b
Nathan <nathan.huesken@posteo.de>
2013-09-02 00:42:42 +0200

    added callbacks for load, save, saveAs in haskell version

--- Main.hs
+++ Main.hs
@@ -3,11 +3,15 @@ import Graphics.UI.WX.Controls
 import Graphics.UI.WXCore
 import Graphics.UI.WXCore.Events
 import TechnicalUtils
+import Debug.Trace
+import Data.String.Utils
+import System.FilePath
+import Control.Exception
 
 main = start mainFrame
 
-data ProgramState = ProgramState { unsavedChanged :: Bool
-                                   , fileName       :: String
+data ProgramState = ProgramState { unsavedChanges :: Bool
+                                   , fileName     :: FilePath
                                    }
 
 mainFrame = do
@@ -19,7 +23,7 @@ mainFrame = do
   open   <- menuItem file [ text := "&Open\tCtrl+O"]
   save   <- menuItem file [ text := "&Save\tCtrl+S"]
   saveAs <- menuItem file [ text := "&Save &as"]
-  quit   <- menuItem file [ text := "&Quit"]
+  quit   <- menuItem file [ text := "&Quit\tCtrl+Q"]
   -- goto menu
   goto    <- menuPane     [ text := "&GoTo"]
   goBack  <- menuItem     goto [ text := "End of recent line with \"&=\"\tCtrl+1"]
@@ -32,11 +36,21 @@ mainFrame = do
                     ,menuBar := [file,goto]]
   buffer   <- textEntry f []
   mainArea <- textCtrl  f [ font := fontFixed]
-  controlOnText mainArea (onTextUpdate mainArea)
-  set f [layout := column 2 [hfill $ widget buffer, fill $ widget mainArea]]
+  controlOnText mainArea (onTextUpdate state mainArea)
+  set f [layout := minsize (sz 300 200) $ column 2 [hfill $ widget buffer, fill $ widget mainArea]]
 
-onTextUpdate :: TextCtrl a -> Var ProgramState -> IO ()
-onTextUpdate tc state = do
+  -- set callbacks
+  set f [on (menu quit)         := onQuit state f mainArea
+        ,on (menu open)         := onOpen state f mainArea
+        ,on (menu save)         := onSave state f mainArea
+        ,on (menu saveAs)       := onSaveAs state f mainArea
+        ,on (menu goToBuffer)   := focusOn buffer
+        ,on (menu goToMainArea) := focusOn mainArea
+        ,on (menu goBack)       := onGoBack mainArea
+        ,on (menu goToEnd)      := onGoToEnd mainArea]
+
+onTextUpdate :: Var ProgramState -> TextCtrl a -> IO ()
+onTextUpdate state tc = do
   -- obtain cursor coordinates
   coord <- textCtrlGetXYInsertionPoint tc
 
@@ -48,7 +62,8 @@ onTextUpdate tc state = do
   textCtrlSetXYInsertionPoint tc coord
 
   -- set state to changed
-  modifyVar (\s -> s {unsavedChanges = True})
+  varUpdate state $ \s -> (s {unsavedChanges = True})
+  return ()
 
 -- Ensure that the text is correctly formated for the buffer
 -- 1. All lines have exactly 40 characters. To many characters are removed, missing characters are filled with spaces
@@ -63,3 +78,117 @@ formatForMainBuffer i = unlines $ (map formatLine $ lines i) ++ missingLines
   numEmptyLinesAtEnd = length $ takeWhile (== True) $ map isEmptyLine (reverse $ lines i)
   missingLines = if numEmptyLinesAtEnd < 4 then replicate (4 - numEmptyLinesAtEnd) emptyLine else []
   emptyLine = replicate 40 ' '
+
+-- Take a functions that takes a text buffer (as lines) and a position in it and returns a new position.
+-- Set this position to the new position in the text buffer
+updateTextBufferPosition :: ((Int,Int) -> [String] -> (Int,Int)) -> TextCtrl a -> IO ()
+updateTextBufferPosition f tc = do
+  -- cursor coordinates and text
+  Point x y <- textCtrlGetXYInsertionPoint tc
+  text  <- get tc text
+  if null text then
+    return ()
+  else do
+    let (newX, newY) = f (x,y) (lines text)
+    textCtrlSetXYInsertionPoint tc (Point newX newY)
+ 
+-- helper function
+lastNonSpaceCharPos :: String -> Int
+lastNonSpaceCharPos = length . dropWhile (==' ') . reverse 
+
+-- goto moste recent line with = in it
+onGoBack :: TextCtrl a -> IO ()
+onGoBack tc = updateTextBufferPosition findRecentLineWithEqualSign tc
+  where
+  findRecentLineWithEqualSign (x,y) ls =
+    let newY = case lastLineWithSymbol '=' (take y ls) of
+             Nothing -> 0
+             Just i  -> i
+        newX = lastNonSpaceCharPos (ls !! newY)
+    in (newX,newY)
+  lastLineWithSymbol :: Char -> [String] -> Maybe Int
+  lastLineWithSymbol sym ls = if potentialNegative < 0 then Nothing else Just potentialNegative
+    where
+    potentialNegative = (length . dropWhile (notElem sym) $ (reverse ls)) - 1
+
+-- go after last symbol in last line
+onGoToEnd :: TextCtrl a -> IO ()
+onGoToEnd tc = updateTextBufferPosition findLastSymbolPosition tc
+  where
+  findLastSymbolPosition _ ls =
+    let newY = boundAtZero $ (length . dropWhile isOnlySpaces $ (reverse ls)) - 1
+        boundAtZero i = if i < 0 then 0 else i
+        isOnlySpaces = and . map (== ' ')
+        newX = lastNonSpaceCharPos (ls !! newY)
+    in (newX,newY)
+
+dealWithUnsavedChanges :: Var ProgramState -> Frame a -> TextCtrl a -> IO Bool
+dealWithUnsavedChanges state f tc = do
+  ProgramState unsavedChanges _ <- varGet state
+  if not unsavedChanges then return True else do
+    res <- messageDialog f "Unsaved changes ..." "You have unsaved changes, do you want to save them?" (wxYES_NO .+. wxCANCEL .+. wxICON_EXCLAMATION)
+    case res of
+      _ | res == wxID_CANCEL -> return False
+        | res == wxID_NO     -> return True
+        | res == wxID_YES    -> do
+          onSave state f tc
+          -- check if saving worked
+          ProgramState newUnsavedChanges _ <- varGet state
+          return (not newUnsavedChanges)
+
+
+onQuit :: Var ProgramState -> Frame a -> TextCtrl a -> IO ()
+onQuit state f tc = do
+    realyQuit <- dealWithUnsavedChanges state f tc
+    when realyQuit (close f)
+  
+
+-- Function to do the actual saving
+saveFile :: TextCtrl a -> Frame a -> FilePath -> IO ()
+saveFile tc f fp = do
+  txt <- get tc text
+  writeFile fp (unlines . reverse . dropWhile null . reverse . map rstrip . lines $ txt) 
+  set f [text := (takeFileName fp)]
+
+openFile :: TextCtrl a -> Frame a -> FilePath -> IO ()
+openFile tc f fp = do
+  txt <- readFile fp
+  set tc [text := txt]
+  set f [text := (takeFileName fp)]
+
+onSaveAs :: Var ProgramState -> Frame a -> TextCtrl a -> IO ()
+onSaveAs state f tc = do
+  ProgramState _ filePath <- varGet state
+  fp <- fileSaveDialog f True True "Select a file to save into ..." [("Text file",["*.txt"])] (dropFileName filePath) (takeFileName filePath)
+  case fp of
+    Nothing -> return ()
+    Just newFp -> do
+      varUpdate state (\s -> s {fileName = newFp, unsavedChanges = False})
+      onSave state f tc
+
+onSave :: Var ProgramState -> Frame a -> TextCtrl a -> IO ()
+onSave state f tc = do
+  ProgramState _ fp <- varGet state
+  if null fp then onSaveAs state f tc
+  else do
+    catch (do
+             saveFile tc f fp
+             varUpdate state (\s -> s {unsavedChanges = False})
+             return ()
+           ) ((\_ -> errorDialog f "Error on writing" ("There was an error writing " ++ fp)) :: SomeException -> IO ())
+  
+onOpen :: Var ProgramState -> Frame a -> TextCtrl a -> IO ()
+onOpen state f tc = do
+  realyOpen <- dealWithUnsavedChanges state f tc
+  ProgramState _ filePath <- varGet state
+  when realyOpen $ do
+    fp <- fileOpenDialog f True True "Select a file to open ..." [("Text file",["*.txt"])] (dropFileName filePath) (takeFileName filePath)
+    case fp of
+      Nothing -> return ()
+      Just newFp -> do
+        catch (do
+                 openFile tc f newFp
+                 onTextUpdate state tc
+                 varUpdate state (\s -> s {fileName = newFp, unsavedChanges = False})
+                 return ()
+              ) $ ((\_ -> errorDialog f "Error on reading" ("There was an error reading " ++ newFp)) :: SomeException -> IO ()) 
